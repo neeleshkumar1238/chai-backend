c16904e64a47376b78770ed619c3abaecb0a6a4a
Neelesh Kumar <neeleshkumar2065@gmail.com>
2025-06-13 16:14:53 +0530

    add register controller and bug fixes

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@
 
 import dotenv from 'dotenv'
 import connectDB from './db/index.js';
-import { MongooseError } from 'mongoose';
-
+import {app} from './app.js'
 dotenv.config({
     path:'./.env'
 })
 
 connectDB()
 .then(()=>{
-    application.listen(process.env.PORT || 8000,()=>{
+    app.listen(process.env.PORT || 8000,()=>{
         console.log(`server is running at port :${process.env.PORT}`);
     })
 })
